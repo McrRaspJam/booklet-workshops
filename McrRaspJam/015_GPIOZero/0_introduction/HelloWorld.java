e50338d143c103419f59b82be97e7ed95e408406
Jack Kelly <jack@jackjkelly.com>
2017-09-01 16:38:21 +0100

    chapter 1 progress

--- McrRaspJam/015_GPIOZero/0_introduction/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class MyClass
-{
-	public static void main(String[] args)
-	{
-		//This is a comment
-		String s = "Text String";
-	}
-}
--- McrRaspJam/015_GPIOZero/0_introduction/led.py
+++ McrRaspJam/015_GPIOZero/0_introduction/led.py
@@ -1,6 +1,4 @@
 from gpiozero import LED
-from time import sleep
-
 led = LED(17)
 
 while True:
