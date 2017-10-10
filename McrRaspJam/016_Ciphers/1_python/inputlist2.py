55cdbf029a4b6acf000dc7ab917037cbd2107265
Jack Kelly <accounts@slightlyintelligent.com>
2017-10-10 23:03:15 +0100

    Section 1

---
+++ McrRaspJam/016_Ciphers/1_python/inputlist2.py
@@ -0,0 +1,4 @@
+text = input("Enter Text: ")
+
+for letter in text:
+    print(letter)
---
+++ McrRaspJam/016_Ciphers/1_python/list.py
@@ -0,0 +1,3 @@
+icecream = ["Vanilla", "Strawberry", "Chocolate", "Raspberry Ripple"]
+print (icecream[0])
+print (icecream[3] + icecream[0] + icecream[1])
